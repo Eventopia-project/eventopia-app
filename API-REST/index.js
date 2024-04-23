47301fe426bc93b360f071a331b7589af3153cff
reboot <moniroka_339@hotmail.com>
2024-04-23 10:15:35 +0100

    starting backend

---
+++ API-REST/index.js
@@ -0,0 +1,5 @@
+// Iniciamos nuestro servidor instanciando express, cors y morgan
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
