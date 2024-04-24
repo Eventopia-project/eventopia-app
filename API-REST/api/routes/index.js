264ebda78ed3a3c7139a945b00f7dc55a5c41136
reboot <moniroka_339@hotmail.com>
2024-04-24 15:32:30 +0100

    adding controllers

--- API-REST/api/routes/index.js
+++ API-REST/api/routes/index.js
@@ -1,7 +1,7 @@
 // Importamos el módulo de enrutamiento de Express y creamos un enrutador
 const router = require('express').Router();
 
-// Usamos el enrutador para manejar todas la peticiones dirigidas a '/user'
+// Usamos el enrutador para manejar todas las peticiones dirigidas a '/user'
 router.use('/user', require('./user.router')); // user.router hace referencia al archivo user.router.js dentro de la carpeta routes
 
 // Usamos el enrutador para manejar todas la peticiones dirigidas a '/event'
@@ -9,6 +9,9 @@ router.use('/event', require('./event.router')); // event.router hace referencia
 
 // Usamos el enrutador para manejar todas la peticiones dirigidas a '/category'
 router.use('/category', require('./category.router')); // category.router hace referencia al archivo user.router.js dentro de la carpeta routes
+
+// Usamos el enrutador para manejar todas las peticiones dirigidas a '/auth'
+router.use('/auth', require('./auth.router'));
 
 // Exportamos el enrutador para que se pueda usar en otros archivos
 module.exports = router;
