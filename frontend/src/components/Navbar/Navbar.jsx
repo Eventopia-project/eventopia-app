97128bea00730bcc59dd7c06a36df926c49eb34f
reboot <moniroka_339@hotmail.com>
2024-04-26 17:04:11 +0100

    adding link router at navbar component

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,25 @@
+import { Link } from 'react-router-dom' 
 import './Navbar.css'
+
 function Navbar() {
-    return (
-        <nav className="nav">
-            <ul className="ul__nav">
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Login</a>
-                </li>
-                <li>
-                    <a href="#">SignUp</a>
-                </li>
-                <li>
-                    <a href="#">Change theme mode</a>
-                </li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className="nav">
+      <ul className="ul__nav">
+        <li>
+          <Link to="#"><button>About</button></Link>
+        </li>
+        <li>
+        <Link to="#"><button>Login</button></Link>
+        </li>
+        <li>
+        <Link to="#"><button>SignUp</button></Link>
+        </li>
+        <li>
+          <button>Change theme mode</button>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
 export default Navbar
