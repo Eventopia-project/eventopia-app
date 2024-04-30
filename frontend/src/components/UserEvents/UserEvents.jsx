9e599e074871fc7dc974e4cafa7304fc531be16f
reboot <moniroka_339@hotmail.com>
2024-04-30 17:17:59 +0100

    code trash

--- frontend/src/components/UserEvents/UserEvents.jsx
+++ frontend/src/components/UserEvents/UserEvents.jsx
@@ -1,18 +1,7 @@
-import { useEffect, useState } from 'react'
 import EventCard from '../EventCard/EventCard'
-import { getProfile } from '../../services/users'
 import './UserEvents.css'
 
 function UserEvents({ events, optionsButton }) {
-  // const [user, setUser] = useState({})
-  // useEffect(() => {
-  //   getProfileData()
-  // }, [userId])
-
-  // const getProfileData = async () => {
-  //   const result = await getProfile()
-  //   setUser(result)
-  // }
   function getFilteredEventsUser() {
     return  events && events.map((event, index) => {
       return <EventCard key={index}
