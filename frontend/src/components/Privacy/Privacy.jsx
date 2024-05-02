6da6830ffc68f80982d33bab82ba479205f38396
Monica-R <moniroka_339@hotmail.com>
2024-05-02 19:26:07 +0100

    styling signup,terms,privacy,about completed

--- frontend/src/components/Privacy/Privacy.jsx
+++ frontend/src/components/Privacy/Privacy.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Privacy.css'
 
 function Privacy() {
   return (
@@ -6,39 +7,39 @@ function Privacy() {
       <h1>Eventopia Application Privacy Policy</h1>
       <ol>
         <li>
-          Introduction: Thank you for choosing our Eventopia Application. Your
+          <span>Introduction:</span> Thank you for choosing our Eventopia Application. Your
           privacy is very important to us and we are committed to protecting it.
           We ask that you read this Privacy Policy to understand what
           information we collect from our users and how we use it.
         </li>
         <li>
-          Information: We Collect We may collect personal information, such as
+          <span>Information:</span> We Collect We may collect personal information, such as
           your name, email address, phone number and location to provide you
           with our application services. We may also collect non-personal
           information, such as device and operating system details, length of
           visit, pages visited, etc.
         </li>
         <li>
-          Use of Information: We use the information we collect to provide you
+          <span>Use of Information:</span> We use the information we collect to provide you
           with our services, improve our application, perform user analytics and
           send marketing communications.
         </li>
         <li>
-          Sharing of information: We will not share your personal information
+          <span>Sharing of information:</span> We will not share your personal information
           with third parties without your consent, except in specific
           circumstances such as compliance with laws and regulations.
         </li>
         <li>
-          Security: We are committed to protecting your information. We use a
+          <span>Security:</span> We are committed to protecting your information. We use a
           variety of security measures to protect your personal information.
         </li>
         <li>
-          Changes to Privacy Policy: We may update our Privacy Policy from time
+          <span>Changes to Privacy Policy:</span> We may update our Privacy Policy from time
           to time. We will notify you of any changes by posting the new Privacy
           Policy on this page.
         </li>
         <li>
-          Contacting Us: If you have any questions about this Privacy Policy, you
+          <span>Contacting Us:</span> If you have any questions about this Privacy Policy, you
           may contact us through the contact information provided in our
           application.
         </li>
--- frontend/src/components/Terms/Terms.jsx
+++ frontend/src/components/Terms/Terms.jsx
@@ -1,40 +1,41 @@
 import React from 'react'
+import './Terms.css'
 
 function Terms() {
   return (
-    <main>
+    <main className='terms'>
       <h1>Terms of Service</h1>
       <p>
-        1. Acceptance of Terms: by using Eventopia, you agree to these Terms of
+        <span>Acceptance of Terms:</span> by using Eventopia, you agree to these Terms of
         Service. If you do not agree to these terms, please do not use our
         application.
       </p>
       <p>
-        Description of Service: Eventopia is an application that provides
+        <span>Description of Service:</span> Eventopia is an application that provides
         information about local events. We are not responsible for any events
         listed in the application.
       </p>
       <p>
-        Use of the Service: you must use Eventopia in accordance with applicable
+        <span>Use of the Service:</span> you must use Eventopia in accordance with applicable
         laws and regulations and must not use the application for illegal
         activities.
       </p>
       <p>
-        User Content Users may post content on Eventopia. You are responsible
+        <span>User Content:</span> Users may post content on Eventopia. You are responsible
         for all content you post and you must have all necessary rights to post
         it.
       </p>
       <p>
-        Property Rights: Eventopia and all related intellectual property rights
+        <span>Property Rights:</span> Eventopia and all related intellectual property rights
         are owned by our company.
       </p>
       <p>
-        Changes to Terms: we may modify these Terms of Service at any time. We
+        <span>Changes to Terms:</span> we may modify these Terms of Service at any time. We
         will notify you of any changes by posting the new Terms of Service on
         this page.
       </p>
       <p>
-        Contact Us: if you have any questions about these Terms of Service, you
+        <span>Contact Us:</span> if you have any questions about these Terms of Service, you
         may contact us through the contact information provided in our
         application.
       </p>
