8f023f5e18204f5f82d6afed48cc7a9d70452b8d
reboot <moniroka_339@hotmail.com>
2024-05-03 13:09:43 +0100

    minimal changes

--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://eventopia-app.onrender.com/',
+    baseURL: 'https://eventopia-app.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
     },
