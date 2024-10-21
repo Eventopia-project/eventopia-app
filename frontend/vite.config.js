d1292e9d34c4a27dbd450c49162463b0a6e53914
Monica-R <moniroka_339@hotmail.com>
2024-10-22 00:53:49 +0100

    watching port and ip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', //escuchando en todas las interfaces
+    port: 3000, // el puerto donde se ejecutará el servidor
+  },
   build: {
       outDir: 'dist',
   }
